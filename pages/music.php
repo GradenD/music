ae678d2f877b4d081d0cebbe0043fcb2d793672a
graden <bacharevia@mail.ru>
2020-12-10 18:22:21 +0500

    music.php 40%

--- pages/music.php
+++ pages/music.php
@@ -6,7 +6,7 @@
 </div>
 <div class="container">
     <div class="popular-songs">
-        <h1>POPULAR <span>NEW</span> SONGS</h1>
+        <h1>INTERESTING NOW</h1>
         <h2>Songs</h2>
         <div id="popular-slider"  class="songs-gallery">
             <div id="songs-content" class="songs-content">
@@ -36,10 +36,10 @@
                 </div>
                 <div class="songs-block">
                     <div class="songs-column">
-                        <img data-src="img/music/markul.jpg">
+                        <img data-src="img/music/let-go.jpg">
                         <div class="info">
                             <h2>Singl</h2>
-                            <h1>Markul - B.I.D</h1>
+                            <h1>Beau Young Prince - Let Go </h1>
                             <div class="social">
                                 <p>July 17, 2016</p>
                                 <p>666<i class="fas fa-heart" aria-hidden="true"></i></p>
@@ -47,10 +47,10 @@
                         </div>
                     </div>
                     <div class="songs-column">
-                        <img data-src="img/music/markus.jpg">
+                        <img data-src="img/music/pharaoh_lune.jpg">
                         <div class="info">
-                            <h2>Album</h2>
-                            <h1>MARKUSPHOENIX- MIRRORS</h1>
+                            <h2>Singl</h2>
+                            <h1>Pharaoh - На луне</h1>
                             <div class="social">
                                 <p>July 17, 2016</p>
                                 <p>666<i class="fas fa-heart" aria-hidden="true"></i></p>
@@ -60,10 +60,10 @@
                 </div>
                 <div class="songs-block">
                     <div class="songs-column">
-                        <img data-src="img/music/markul.jpg">
+                        <img data-src="img/music/masrk-skall.jpg">
                         <div class="info">
                             <h2>Singl</h2>
-                            <h1>Markul - B.I.D</h1>
+                            <h1>Markul - Скалы</h1>
                             <div class="social">
                                 <p>July 17, 2016</p>
                                 <p>666<i class="fas fa-heart" aria-hidden="true"></i></p>
@@ -71,34 +71,10 @@
                         </div>
                     </div>
                     <div class="songs-column">
-                        <img data-src="img/music/markus.jpg">
+                        <img data-src="img/music/obladaet.jpg">
                         <div class="info">
                             <h2>Album</h2>
-                            <h1>MARKUSPHOENIX- MIRRORS</h1>
-                            <div class="social">
-                                <p>July 17, 2016</p>
-                                <p>666<i class="fas fa-heart" aria-hidden="true"></i></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="songs-block">
-                    <div class="songs-column">
-                        <img data-src="img/music/markul.jpg">
-                        <div class="info">
-                            <h2>Singl</h2>
-                            <h1>Markul - B.I.D</h1>
-                            <div class="social">
-                                <p>July 17, 2016</p>
-                                <p>666<i class="fas fa-heart" aria-hidden="true"></i></p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="songs-column">
-                        <img data-src="img/music/markus.jpg">
-                        <div class="info">
-                            <h2>Album</h2>
-                            <h1>MARKUSPHOENIX- MIRRORS</h1>
+                            <h1>Obladaet- 3D19</h1>
                             <div class="social">
                                 <p>July 17, 2016</p>
                                 <p>666<i class="fas fa-heart" aria-hidden="true"></i></p>
@@ -112,6 +88,70 @@
             </div>
             <div id="popular-right" class="popular-right row">
                 <i class="fas fa-chevron-right" aria-hidden="true"></i>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="new-relize">
+        <h1>NEW RELEASES</h1>
+        <h2>New tracks, albums and compilations</h2>
+        <div class="relize-content">
+            <div class="relize-column">
+                <img data-src="img/music/markul.jpg">
+                <div class="info">
+                    <h1>Markul - B.I.D</h1>
+                    <p>Singl</p>
+                </div>
+            </div>
+            <div class="relize-column">
+                <img src="img/music/markus.jpg">
+                <div class="info">
+                    <h1>MARKUSPHOENIX- MIRRORS</h1>
+                    <p>Album</p>
+                </div>
+            </div>
+            <div class="relize-column">
+                <img src="img/music/let-go.jpg">
+                <div class="info">
+                    <h1>Beau Young Prince - Let Go </h1>
+                    <p>Singl</p>
+                </div>
+            </div>
+            <div class="relize-column">
+                <img src="img/music/pharaoh_lune.jpg">
+                <div class="info">
+                    <h1>Pharaoh - На луне</h1>
+                    <p>Singl</p>
+                </div>
+            </div>
+            <div class="relize-column">
+                <img src="img/music/masrk-skall.jpg">
+                <div class="info">
+                    <h1>Markul - Скалы</h1>
+                    <p>Singl</p>
+                </div>
+            </div>
+            <div class="relize-column">
+                <img src="img/music/obladaet.jpg">
+                <div class="info">
+                    <h1>Obladaet- 3D19</h1>
+                    <p>Album</p>
+                </div>
+            </div>
+            <div class="relize-column">
+                <img data-src="img/music/markul.jpg">
+                <div class="info">
+                    <h1>Markul - B.I.D</h1>
+                    <p>Singl</p>
+                </div>
+            </div>
+            <div class="relize-column">
+                <img src="img/music/markus.jpg">
+                <div class="info">
+                    <h1>MARKUSPHOENIX- MIRRORS</h1>
+                    <p>Album</p>
+                </div>
             </div>
         </div>
     </div>
